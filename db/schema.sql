c6470e1d6c45dc9700714eb86dac16d68ce09e41
sdsautter <sdsautter@gmail.com>
2017-06-03 08:43:50 -0400

    Took out unncessarily files

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,1 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers(
-id INT(50) AUTO_INCREMENT NOT NULL,
-burger_name VARCHAR(100) NOT NULL,
-devoured BOOLEAN NOT NULL DEFAULT 0,
-PRIMARY KEY(id)
-);
+CREATE DATABASE sequelizedburgers;
--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-USE burgers_db;
-
-INSERT INTO burgers(burger_name) VALUES ("All-Out Burger");
-INSERT INTO burgers(burger_name) VALUES ("Sweet Sassy Molassey Burger");
-INSERT INTO burgers(burger_name) VALUES ("Barbecue Hot-N-Tips Burger");
